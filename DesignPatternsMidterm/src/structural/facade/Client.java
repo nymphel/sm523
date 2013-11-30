cb16863b05d6ff56223d19e7247d532ab232b5ee
rylai <rylai@rylai-PC>
2013-12-01 00:26:25 +0200

    ry

--- DesignPatternsMidterm/src/structural/facade/Client.java
+++ DesignPatternsMidterm/src/structural/facade/Client.java
@@ -3,6 +3,7 @@ package structural.facade;
 public class Client {
 
 	public static void main(String[] args) {
+		
 		OrderFacade orderFacade = new OrderFacade();
 		orderFacade.placeOrder("OR123456");
 		System.out.println("Order processing completed");
