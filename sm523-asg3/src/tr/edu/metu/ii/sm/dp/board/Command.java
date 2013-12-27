47a8a56c9fd294a20fc7e2666bb2eef45a24dc7f
nymphel <nymphel>
2013-12-27 09:42:59 +0200

    command

---
+++ sm523-asg3/src/tr/edu/metu/ii/sm/dp/board/Command.java
@@ -0,0 +1,8 @@
+package tr.edu.metu.ii.sm.dp.board;
+
+//Command
+public interface Command {
+	
+	public void execute();
+
+}
---
+++ sm523-asg3/src/tr/edu/metu/ii/sm/dp/board/MoveToBeginningCommand.java
@@ -0,0 +1,12 @@
+package tr.edu.metu.ii.sm.dp.board;
+
+//Concrete Command
+public class MoveToBeginningCommand implements Command {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
