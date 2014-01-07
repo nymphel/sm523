7c037a373e0a88622a7e41a6a1ec3fa2b6692014
rylai <rylai@rylai-PC>
2014-01-08 00:52:43 +0200

    state

---
+++ DesignPatternsMidterm/src/behavioral/state/Client.java
@@ -0,0 +1,18 @@
+package behavioral.state;
+
+public class Client {
+
+	public static void main(String[] args) {
+		TVContext context = new TVContext();
+		State tvStartState = new TVStartState();
+		State tvStopState = new TVStopState();
+
+		context.setState(tvStartState);
+		context.doAction();
+
+		context.setState(tvStopState);
+		context.doAction();
+
+	}
+
+}
